b5c440957347c9283220be96b4952f1b40edd4a1
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2019-10-26 00:00:42 +0300

    misc job

---
+++ Source/ReligionsOfRimworld/ReligionsOfRimworld/RoomRoleWorker_Temple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace ReligionsOfRimworld
+{
+    public class RoomRoleWorker_Temple : RoomRoleWorker
+    {
+        public override float GetScore(Room room)
+        {
+            int num = 0;
+            List<Thing> andAdjacentThings = room.ContainedAndAdjacentThings;
+            for (int index = 0; index < andAdjacentThings.Count; ++index)
+            {
+                if (andAdjacentThings[index] is Building_ReligiousBuildingMain)
+                {
+                    ++num;
+                }
+            }
+            return (float)num * 7.6f;
+        }
+    }
+}
