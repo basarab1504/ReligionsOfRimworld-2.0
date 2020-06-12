e858d9c861a3c41e88d27c3675668e89427e65dc
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2020-06-12 18:33:21 +0300

    pray alone def

---
+++ Source/ReligionsOfRimworld/ReligionsOfRimworld/JobDriver_PrayAlone.cs
@@ -0,0 +1,106 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
+using Verse.AI;
+
+namespace ReligionsOfRimworld
+{
+	public class JobDriver_RelaxAlone : JobDriver
+	{
+		private Rot4 faceDir = Rot4.Invalid;
+
+		private const TargetIndex SpotOrBedInd = TargetIndex.A;
+
+		private bool FromBed
+		{
+			get
+			{
+				return this.job.GetTarget(TargetIndex.A).HasThing;
+			}
+		}
+
+		public override bool CanBeginNowWhileLyingDown()
+		{
+			return this.FromBed && JobInBedUtility.InBedOrRestSpotNow(this.pawn, this.job.GetTarget(TargetIndex.A));
+		}
+
+		public override bool TryMakePreToilReservations(bool errorOnFailed)
+		{
+			if (this.FromBed)
+			{
+				Pawn pawn = this.pawn;
+				LocalTargetInfo target = this.job.GetTarget(TargetIndex.A);
+				Job job = this.job;
+				int sleepingSlotsCount = ((Building_Bed)this.job.GetTarget(TargetIndex.A).Thing).SleepingSlotsCount;
+				int stackCount = 0;
+				if (!pawn.Reserve(target, job, sleepingSlotsCount, stackCount, null, errorOnFailed))
+				{
+					return false;
+				}
+			}
+			else
+			{
+				Pawn pawn = this.pawn;
+				LocalTargetInfo target = this.job.GetTarget(TargetIndex.A);
+				Job job = this.job;
+				if (!pawn.Reserve(target, job, 1, -1, null, errorOnFailed))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		[DebuggerHidden]
+		protected override IEnumerable<Toil> MakeNewToils()
+		{
+			Toil relax;
+			if (this.FromBed)
+			{
+				this.KeepLyingDown(TargetIndex.A);
+				yield return Toils_Bed.ClaimBedIfNonMedical(TargetIndex.A, TargetIndex.None);
+				yield return Toils_Bed.GotoBed(TargetIndex.A);
+				relax = Toils_LayDown.LayDown(TargetIndex.A, true, false, true, true);
+				relax.AddFailCondition(() => !this.pawn.Awake());
+			}
+			else
+			{
+				yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
+				relax = new Toil();
+				relax.initAction = delegate
+				{
+					this.faceDir = ((!this.job.def.faceDir.IsValid) ? Rot4.Random : this.job.def.faceDir);
+				};
+				relax.handlingFacing = true;
+			}
+			relax.defaultCompleteMode = ToilCompleteMode.Delay;
+			relax.defaultDuration = this.job.def.joyDuration;
+			relax.AddPreTickAction(delegate
+			{
+			if (this.faceDir.IsValid)
+				{
+				this.pawn.rotationTracker.FaceCell(this.pawn.Position + this.faceDir.FacingCell);
+			}
+			this.pawn.GainComfortFromCellIfPossible();
+			JoyUtility.JoyTickCheckEnd(this.pawn, JoyTickFullJoyAction.EndJob, 1f, null);
+		});
+			yield return relax;
+
+			this.AddFinishAction(() =>
+			{
+				ReligionProperty property = pawn.GetReligionComponent().Religion.GetSettings<ReligionSettings_Prayings>(SettingsTagDefOf.PrayingsTag).Property;
+				PietyUtility.TryApplyOnPawn(property.Subject, pawn);
+			});
+		}
+
+
+
+	public override void ExposeData()
+	{
+		base.ExposeData();
+		Scribe_Values.Look<Rot4>(ref this.faceDir, "faceDir", default(Rot4), false);
+	}
+}
+}
